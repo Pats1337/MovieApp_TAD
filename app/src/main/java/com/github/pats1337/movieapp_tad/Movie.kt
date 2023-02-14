3df3a92bd1bec0a183287cdb19079a3f27c08e18
Alex Terentev <Ater1337@yandex.ru>
2023-02-14 13:39:50 +0300

    completed task 1.1

---
+++ app/src/main/java/com/github/pats1337/movieapp_tad/Movie.kt
@@ -0,0 +1,10 @@
+package com.github.pats1337.movieapp_tad
+
+data class Movie(
+    val id: Int,
+    val title: Int,
+    val cover: Int,
+    val releaseDate: Int,
+    val rating: Int,
+    val description: Int
+)
