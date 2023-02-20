412c954685e91658a5ed16556162f5d993b65f75
Alex Terentev <Ater1337@yandex.ru>
2023-02-20 14:05:15 +0300

    completed task 1.2

--- app/src/main/java/com/github/pats1337/movieapp_tad/Movie.kt
+++ app/src/main/java/com/github/pats1337/movieapp_tad/Movie.kt
@@ -1,10 +1,17 @@
 package com.github.pats1337.movieapp_tad
 
+import androidx.annotation.StringRes
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Movie(
     val id: Int,
-    val title: Int,
+    @StringRes val title: Int,
     val cover: Int,
     val releaseDate: Int,
     val rating: Int,
-    val description: Int
-)
+    @StringRes val description: Int
+) : Parcelable {
+
+}
